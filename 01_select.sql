e02c3e4b703afd461b4d77c0d3fcdfdc6b8a4ce2
jinyong Kim <jyjy0509@naver.com>
2024-02-15 15:43:45 +0900

    initial commit

---
+++ 01_select.sql
@@ -0,0 +1,19 @@
+SELECT 
+       MENU_NAME
+  FROM
+       TBL_MENU
+
+Select
+        menu_code
+	   ,menu_name
+	   ,menu_price
+  FROM tbl_menu;
+  
+SELECT
+      *
+  FROM TBL_MENU;
+SELECT 6+3;
+SELECT 6*3;
+SELECT NOW();
+SELECT CONTAT('홍','','길동') AS NAME;
+SELECT CONTAT('홍','','길동') AS FULLNAME;
